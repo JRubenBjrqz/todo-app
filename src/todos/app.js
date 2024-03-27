243e40d96ea0b0a17c4dcacd83a01be6013fc84e
Rubén Bojórquez <159498245+jrubenbjrqzo@users.noreply.github.com>
2024-03-26 21:58:46 -0700

    First commit template

---
+++ src/todos/app.js
@@ -0,0 +1,14 @@
+import html from './app.html?raw'
+
+/**
+ * @param {String} elementId 
+ */
+export const App = ( elementId ) => {
+
+    // When function App() calls itself
+    (() => {
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector(elementId).append(app);
+    })();
+}
